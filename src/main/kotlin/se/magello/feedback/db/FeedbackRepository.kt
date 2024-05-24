6ddb4e914e4673905adb773e95b237f787932c03
Dag Reuterskiöld <dsterna@kth.se>
2024-05-24 11:06:23 +0200

    removed old stack

--- src/main/kotlin/se/magello/feedback/db/FeedbackRepository.kt
+++
@@ -1,8 +0,0 @@
-package se.magello.feedback.db
-
-import org.springframework.data.repository.CrudRepository
-import se.magello.feedback.model.internal.Feedback
-
-interface FeedbackRepository : CrudRepository<Feedback, Int> {
-
-}
